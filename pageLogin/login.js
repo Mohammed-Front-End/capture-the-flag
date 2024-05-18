9f90abb3f38cfcf037122268626c45743b0f2d56
mohabbro <elabedymidoxl@gmail.com>
2024-05-18 13:14:42 +0300

    add from login

---
+++ pageLogin/login.js
@@ -0,0 +1,6 @@
+  // Event listener for the register button
+  document.getElementById('register-btn').addEventListener('click', (e) => {
+    e.preventDefault()
+    window.location.href = 'https://sh3ll.cloud/xf2/forums/68/';
+  });
+  
